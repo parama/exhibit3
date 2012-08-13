738bd4104a68f5ea87d296fec1c70dde04a11c87
Parama Pal <paramap@gmail.com>
2012-08-13 10:46:18 -0400

    updated .project file

---
+++ scripted/demos/cv/cv.js
@@ -0,0 +1,73 @@
+{
+    "types": {
+        "Project" : {
+            "pluralLabel": "Projects"
+        },
+		"Coursework" : {
+            "pluralLabel": "Coursework"
+        }
+    },
+    "items" : [
+        {   "type":                  "Project",
+            "label":                 "Error Concealment Algorithm",
+            "discipline":            "Signal Processing",
+            "lab":            "Ozmo Devices",
+			"location": "Palo Alto, CA",
+            "year":          "2008",
+			"collaborators": 	"Sriram Sundarajan", 
+            "imageURL":              "images/pic_digitalsignal_thumb.jpg"
+        },
+        {   "type":                  "Project",
+            "label":                 "Novel Methods of fMRI Data Analysis",
+            "lab":            "CSAIL",
+            "discipline":            "Medical Imaging",
+			"location": "Cambridge, MA", 
+            "year":          "2009",
+			"collaborators": 	["Polina Golland","Danial Lashkari"],
+            "imageURL":              "images/dti_thumb.jpg"
+        },
+        {   "type":                  "Project",
+            "label":                 "Optimizing a Diffusion Tensor Imaging Image Processing Pipeline",
+            "lab":            "SCSNL",
+            "discipline":            "Medical Imaging",
+			"location": "Stanford, CA",
+			"collaborators": 	"Vinod Menon",
+            "year":          "2007",
+            "imageURL":              "images/dti_thumb.jpg"
+        },
+        {   "type":                  "Project",
+            "label":                 "Exhibit: An Accesible Information Visualization Library",
+            "lab":            "CSAIL",
+            "discipline":            "Information Visualization",
+			"location": "Cambridge, MA",
+            "year":          "2011",
+            "imageURL":              "images/exhibit_thumb.png"
+        },
+        {   "type":                  "Coursework",
+            "label":                 "Ontology Viewer: Visualizing Semantic Data",
+            "discipline":            "Information Visualization",
+			"location": "Cambridge, MA",
+			"collaborators": 	["Gil Alterovitz", "Peter Solovitz", "Amy Wooten"], 
+            "year":          "2010",
+            "imageURL":              "images/ontology_viewer_thumb.jpg"
+        },
+        {   "type":                  "Coursework",
+            "label":                 "Interactively Visualizing Event Listings: ConcertBOS",
+            "lab":            "6.813",
+            "discipline":            "Information Visualization",
+			"location": "Cambridge, MA",
+			"collaborators": 	["Rob Miller", "Katrina Panovich", "Kiran Bhattaram", "Evan Thomas"], 
+            "year":          "2010",
+            "imageURL":              "images/cb_friends_thumb.png"
+        },
+        {   "type":                  "Project",
+            "label":                 "Tracking the Flow of Attentional Mechanisms",
+             "lab":            "Desimone Lab",
+	            "discipline":            "Neuroscience",
+				"location": "Cambridge, MA",
+				"collaborators": 	["Bob Desimone", "Grant Pielli"], 
+	            "year":          "2006",
+	            "imageURL":              "images/waldo_thumb.jpg"
+        }
+    ]
+}
